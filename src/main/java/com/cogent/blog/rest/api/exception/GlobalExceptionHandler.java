5ce971f5732e0cd7cb773df1f112ea7249326dd6
Kai Griem <kgriem@catalyte.io>
2024-08-02 12:53:41 -0700

    errors returned in data object

--- src/main/java/com/cogent/blog/rest/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cogent/blog/rest/api/exception/GlobalExceptionHandler.java
@@ -1,16 +1,23 @@
 package com.cogent.blog.rest.api.exception;
 
 import com.cogent.blog.rest.api.payload.ErrorDetail;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetail> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
         ErrorDetail errorDetail=new ErrorDetail(new Date(), exception.getMessage(), webRequest.getDescription(false));
@@ -21,5 +28,27 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorDetail> handleRuntimeExcetion(BlogApiException exception, WebRequest webRequest){
         ErrorDetail errorDetail=new ErrorDetail(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetail> handleGlobalException(Exception ex, WebRequest webRequest) {
+        ErrorDetail errorDetail = new ErrorDetail(
+                new Date(),
+                ex.getMessage(),
+                webRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetail, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode Status, WebRequest request){
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error)->{
+            FieldError fieldError = (FieldError) error;
+            String fieldName = fieldError.getField();
+            String message = fieldError.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
